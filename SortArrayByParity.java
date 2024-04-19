43e84a892dae6f763bd5e364d7effbc83a4891c6
praveenkumar2661 <praveenkumar1856399@gmail.com>
2024-04-19 20:01:41 +0530

    LeetCode

--- SortArrayByParity.java
+++ SortArrayByParity.java
@@ -4,14 +4,17 @@ public class SortArrayByParity
 {
 	public int[] sortArrayByParity(int[] nums) 
     {
-        int[] arr = new int[nums.length];
-        int y = 0;
-        for(int i = 0; i < nums.length; i++)
-            if(nums[i]%2 == 0)
-                arr[y++] = nums[i];
-        for(int i = 0; i < nums.length; i++)
-            if(nums[i]%2 != 0)
-                arr[y++] = nums[i];
-        return arr;
+		int count = 0;
+	       for(int i = 0; i < nums.length; i++)
+	        {
+	            if(nums[i]%2 == 0)
+	            {
+	                int temp = nums[i];
+	                nums[i] = nums[count];
+	                nums[count] = temp;
+	                count++;
+	            }
+	        }
+	        return nums;
     }
 }
