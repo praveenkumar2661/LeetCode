146d1b4ae4c7588493b1045ba70d91fb368fb0ef
praveenkumar2661 <praveenkumar1856399@gmail.com>
2024-04-24 23:44:23 +0530

    LeetCode

---
+++ ValidParentheses.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class ValidParentheses 
+{
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else if (c == ')' && !stack.isEmpty() && stack.peek() == '(')
+                stack.pop();
+            else if (c == '}' && !stack.isEmpty() && stack.peek() == '{')
+                stack.pop();
+            else if (c == ']' && !stack.isEmpty() && stack.peek() == '[')
+                stack.pop();
+        }
+        return stack.isEmpty();
+    }
+}
